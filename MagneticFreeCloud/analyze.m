c59cd51a5c76a5dda1097fb8226e108371bb69f1
Ben Roque <broque@clarku.edu>
2018-04-14 08:49:52 -0400

    new run

--- MagneticFreeCloud/analyze.m
+++ MagneticFreeCloud/analyze.m
@@ -2,5 +2,12 @@ function analyze(filename)
     raw=importdata(filename);
     x=raw(:,1);
     y=raw(:,2);
-    plot(x,y);
+    x1=x(1:2:end);
+    x2=x(2:2:end);
+    y1=y(1:2:end);
+    y2=y(2:2:end);
+    plot(x1,y1,x2,y2)
+%     xdif=x1-x2;
+%     ydif=y1-y2;
+%     plot(xdif,ydif)
 end
